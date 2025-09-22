d7f3f2bc2cb3bd43a0aa5160b6dd68520e656e08
peterbecskei <peter.becskei@gmail.com>
2025-09-22 07:28:11 +0200

    h

--- background.js
+++ background.js
@@ -2,24 +2,52 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed');
 });
-
+ url_csv = [];
 // Message listener for content scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "saveLinks") {
-    saveLinksToCSV(request.links);
+  if (request.action === "saveContent") {
+    //saveLinksToCSV(request.text);
     sendResponse({status: "success"});
   }
   return true;
 });
 
-function saveLinksToCSV(links) {
-  const csvContent = "URL\n" + links.map(link => `"${link}"`).join("\n");
-  const blob = new Blob([csvContent], { type: 'text/csv' });
-  const url = URL.createObjectURL(blob);
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message.action === "saveContent") {
+    // Fájlnév az URL alapján
+    let safeName = message.url.replace(/[^a-z0-9]/gi, "_").toLowerCase();
+    //let filename = safeName + ".txt";
+    saveLinkToLocalStore(safeName);
+    //   chrome.downloads.download({
+ //     url: URL.createObjectURL(
+   //     new Blob([message.text], { type: "text/plain" })
+  //    ),
+ //     filename: filename,
+ //     saveAs: false
+ //   });
+  }
+});
 
-  chrome.downloads.download({
-    url: url,
-    filename: 'HREF.csv',
-    saveAs: true
-  });
+
+function saveLinkToLocalStore(links) {
+  // Retrieve existing list from local storage or initialize a new one
+ // let url_csv = JSON.parse(localStorage.getItem('URL_DATA')) || [];
+
+  // Append the links parameter to the list
+  url_csv.push(links);
+
+  // Save the updated list back to local storage with key URL_DATA
+  chrome.storage.local.set('URL_DATA', JSON.stringify(url_csv));
 }
+
+
+//function saveLinksToCSV(links) {
+//  const csvContent = "URL\n" + links.map(link => `"${link}"`).join("\n");
+//  const dataUrl = "data:text/csv;base64," + btoa(unescape(encodeURIComponent(csvContent)));
+
+//  chrome.downloads.download({
+//    url: dataUrl,
+ //   filename: 'HREF.csv',
+ //   saveAs: true
+//  });
+//}
